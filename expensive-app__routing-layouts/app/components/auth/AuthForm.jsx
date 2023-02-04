7b5303cc28ad840917d8f9161734d1f03b994822
Richelly Italo <richellyitalo@gmail.com>
2023-02-04 16:15:31 -0300

    feat: form element according mode

--- expensive-app__routing-layouts/app/components/auth/AuthForm.jsx
+++ expensive-app__routing-layouts/app/components/auth/AuthForm.jsx
@@ -1,22 +1,48 @@
-import { FaLock } from 'react-icons/fa';
+import { Link, useSearchParams } from "@remix-run/react";
+import { FaLock, FaPlus } from "react-icons/fa";
 
 function AuthForm() {
+  const [searchParams] = useSearchParams();
+
+  const authMode = searchParams.get("mode") || "login";
+  const isLogin = authMode === "login";
+  const FormHeaderIcon = isLogin ? FaLock : FaPlus;
+  const textSubmit = isLogin ? "Login" : "Sign Up";
+
   return (
-    <form method="post" className="form" id="auth-form">
+    <form
+      method="post"
+      className="form"
+      id="auth-form"
+    >
       <div className="icon-img">
-        <FaLock />
+        <FormHeaderIcon />
       </div>
       <p>
         <label htmlFor="email">Email Address</label>
-        <input type="email" id="email" name="email" required />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          required
+        />
       </p>
       <p>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" minLength={7} />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          minLength={7}
+        />
       </p>
       <div className="form-actions">
-        <button>Login</button>
-        <a href="/auth">Log in with existing user</a>
+        <button>{textSubmit}</button>
+        {isLogin ? (
+          <Link to="?mode=signup">Create a new User</Link>
+        ) : (
+          <Link to="?mode=login">Log in with existing user</Link>
+        )}
       </div>
     </form>
   );
